ebfabd4c8d23cdbfe5cdf5090cae05a195ea03b0
jevonthomas <jevon.g.thomas@gmail.com>
2017-08-18 00:20:15 -0500

    a lot of styling

--- app/controllers/choose-exercise-controller.js
+++ app/controllers/choose-exercise-controller.js
@@ -13,31 +13,33 @@ workoutJournalApp.controller("ChooseExercisesController", function($scope, $wind
 
   //Allows fetchExercises to get the selected muscle group from the previous window
   //Used to display the workout profile at the top of the page
-  $scope.workoutArr = [];
   function fetchSingleWorkout(workoutURL) {
-      WorkoutJournalFactory.getSingleWorkout(workoutURL)
-      .then( (workout) => {
-        $scope.workoutArr.push(workout.data);
-      })
-      .catch( (err) => {
-          console.log("error", err);
-      });
+    let workoutArray = [];
+    WorkoutJournalFactory.getSingleWorkout(workoutURL)
+    .then( (workout) => {
+      workoutArray.push(workout.data);
+      $scope.workoutArr = workoutArray;
+    })
+    .catch( (err) => {
+        console.log("error", err);
+    });
   }
 
   //Calls a get function from wj factory to get exercises
   //based on the value of the key muscle-group
-  $scope.exercises = [];
   $scope.fetchExercises = (muscleGroup) => {
-      WorkoutJournalFactory.getExercises(muscleGroup)
-      .then( (exerciseList) => {
-        let exerciseData = exerciseList.data;
-        Object.keys(exerciseData).forEach( (key) => {
-            $scope.exercises.push(exerciseData[key]);
-        });
-      })
-      .catch( (err) => {
-          console.log("error", err);
+    let exercisesArr = [];
+    WorkoutJournalFactory.getExercises(muscleGroup)
+    .then( (exerciseList) => {
+      let exerciseData = exerciseList.data;
+      Object.keys(exerciseData).forEach( (key) => {
+        exercisesArr.push(exerciseData[key]);
       });
+      $scope.exercises = exercisesArr;
+    })
+    .catch( (err) => {
+      console.log("error", err);
+    });
   };
 
   //Takes the user to the page to plan their sets
